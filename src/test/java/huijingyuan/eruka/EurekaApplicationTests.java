cbcba562385376bbabb355dcda629f2f7e3453b3
cheeser <649457551@qq.com>
2020-04-13 23:30:39 +0800

    eureka服务器

---
+++ src/test/java/huijingyuan/eruka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package huijingyuan.eruka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
